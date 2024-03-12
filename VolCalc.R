a8089745d0d80378ab448e6375286f1ff648a603
Naomi <naomipremlim@gmail.com>
2024-03-12 23:57:13 +0800

    ACCURATE CALCULATIONS !! :D

--- VolCalc.R
+++ VolCalc.R
@@ -35,7 +35,7 @@ for (rowname in files) {
   colnames(df2)<- c("Std_CH4_Peak","Std_N2O_Peak","Std_CH4_PPM","Std_N2O_PPM","CH4_Sample_Peak","N2O_Sample_Peak","Sample_Type","GC_Run","Date","Plot","Time","Chamber_Temp_C","Headspace1_cm","Headspace2_cm","Headspace3_cm","Headspace4_cm","Extension_ft","Chamber_Radius","HeadspaceMean_cm","Extension_cm","Chamber_Height","Total_Volume_cm3","Total_Volume_L","Base_Area","Base_Area_m3","VolumeRatio")
   
   
-  write_xlsx(df2,file.path(getwd(), "VolumeExport.xlsx"))
+  ##write_xlsx(df2,file.path(getwd(), "VolumeExport.xlsx"))
   
 
   #generating linear relation
@@ -93,8 +93,8 @@ for (rowname in files) {
   colnames(df_out)[32] <- "CH4_Concentration"
 
   
-  exportdirect <- paste0("Exports/",batch,"/","MainExport")
-  write_xlsx(df_out,file.path(getwd(), "MainExport.xlsx"))
+
+  #write_xlsx(df_out,file.path(getwd(), "MainExport.xlsx"))
   
   
 
@@ -136,7 +136,7 @@ for (rowname in files) {
     vectorN <- c(vectorN, flux[row, "N20Conc"])
     vectorC <- c(vectorC, flux[row, "CH4Conc"])
     
-    
+   
     if ( counter ==0 ){
       leftboundN = flux[row, "N20Conc"]
       leftboundC = flux[row, "CH4Conc"]
@@ -172,7 +172,10 @@ for (rowname in files) {
     
     
     # Detection Test for 0,63
+    # Hit the end of the row
     if ( counter ==3 ){
+      
+      
       finalN = abs( leftboundN - flux[row, "N20Conc"])
       finalC = abs( leftboundC - flux[row, "CH4Conc"])
       if(finalN <0.000183){
@@ -188,6 +191,7 @@ for (rowname in files) {
       counter =0
       
       #4 Point Linearity Test
+    
       
       Nrsq<-  summary(lm(vectorN~time))$r.squared
       Crsq<-  summary(lm(vectorC~time))$r.squared
@@ -195,10 +199,11 @@ for (rowname in files) {
       Ngrad <-coef(lm(vectorN~time))[2]
       Cgrad <-coef(lm(vectorC~time))[2]
       
-      
+   
       if(Nrsq >0.845){
         NLin ="LIN"
       }else{
+        
         #If 4pt Lin test fail
         #START 3 POINT LINEARITY TEST
         
@@ -207,34 +212,41 @@ for (rowname in files) {
         vectorN1 <- vectorN[-c(4)]
         time1 <- time[-c(4)]
         Nrsq1<-  summary(lm(vectorN1~time1))$r.squared
-        Ngrad1 <- coef(lm(vectorN1~time1))
+        Ngrad1 <- coef(lm(vectorN1~time1))[2]
         
         
         #1,2,3
         vectorN2 <- vectorN[-c(1)]
         time2 <- time[-c(1)]
         Nrsq2<-  summary(lm(vectorN2~time2))$r.squared
-        Ngrad2 <- coef(lm(vectorN2~time2))
+        Ngrad2 <- coef(lm(vectorN2~time2))[2]
         
         #0,1,3
         vectorN3 <- vectorN[-c(2)]
         time3 <- time[-c(2)]
         Nrsq3<-  summary(lm(vectorN3~time3))$r.squared
-        Ngrad3 <- coef(lm(vectorN3~time3))
+        Ngrad3 <- coef(lm(vectorN3~time3))[2]
         
         #0,2,3
         
         vectorN4 <- vectorN[-c(3)]
         time4 <- time[-c(3)]
         Nrsq4<-  summary(lm(vectorN4~time4))$r.squared
-        Ngrad4 <- coef(lm(vectorN4~time4))
+        Ngrad4 <- coef(lm(vectorN4~time4))[2]
+       
         
-        
+        Ngrads <- c(Ngrad1,Ngrad2,Ngrad3,Ngrad4)
         Nrsqs <- c(Nrsq1,Nrsq2,Nrsq3,Nrsq4)
         MaxrsqN <- max(Nrsqs)
+        
+        #Get Highest RSQ Index
+        MaxrsqNindex <- which.max(Nrsqs)
         #Take Highest RSQ, if fail again, then truly failed Linearity Test
         if(MaxrsqN > 0.8545){
+          
           NLin= "LIN"
+          Nrsq <- MaxrsqN
+          Ngrad <- Ngrads[MaxrsqNindex]
         }else{
           NLin ="MISS"
           Ngrad=0
@@ -249,35 +261,51 @@ for (rowname in files) {
         #START 3 POINT LINEARITY TEST
         
         #0,1,2
-        
+     
         vectorC1 <- vectorC[-c(4)]
         time1 <- time[-c(4)]
         Crsq1<-  summary(lm(vectorC1~time1))$r.squared
-        
-        
+        Cgrad1 <- coef(lm(vectorC1~time1))[2]
+
         #1,2,3
         vectorC2 <- vectorC[-c(1)]
         time2 <- time[-c(1)]
         Crsq2<-  summary(lm(vectorC2~time2))$r.squared
+        Cgrad2 <- coef(lm(vectorC2~time2))[2]
         
         #0,1,3
         vectorC3 <- vectorC[-c(2)]
         time3 <- time[-c(2)]
         Crsq3<-  summary(lm(vectorC3~time3))$r.squared
+        Cgrad3 <- coef(lm(vectorC3~time3))[2]
         
         #0,2,3
         
         vectorC4 <- vectorC[-c(3)]
         time4 <- time[-c(3)]
         Crsq4<-  summary(lm(vectorC4~time4))$r.squared
+        Cgrad4 <- coef(lm(vectorC4~time4))[2]
         
         
+        
+        Cgrads <- c(Cgrad1,Cgrad2,Cgrad3,Cgrad4)
         
         Crsqs <- c(Crsq1,Crsq2,Crsq3,Crsq4)
         MaxrsqC <- max(Crsqs)
         
+        MaxrsqCindex <- which.max(Crsqs)
+        
+        
         if(MaxrsqC > 0.8545){
           CLin= "LIN"
+          Crsq <- MaxrsqC
+          print("EDITED GRADIENT")
+          
+          Cgrad <- Cgrads[MaxrsqCindex]
+          print(Cgrad)
+          print ("INDEX THAT SUCCEEDED IS ")
+          print(MaxrsqCindex)
+
         }else{
           CLin ="MISS"
           Cgrad=0
@@ -286,7 +314,6 @@ for (rowname in files) {
       
       plotno <- flux[row, "Plot"]
       dateno <- format(flux[row, "Date"])
-      
       volumeratio <-flux[row,"VolumeRatio"]
       
       
@@ -298,6 +325,8 @@ for (rowname in files) {
       if (DetectionBoolC == TRUE){
         Cgrad=0
       }
+      
+      
       
       Cflux <- Cgrad*volumeratio*240*60
       Nflux <- Ngrad*volumeratio*240*60
